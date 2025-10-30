acf94651430e57a95ef980a2b707fafc2c8247eb
Jorge López Blasco <115549197+jorloicono@users.noreply.github.com>
2025-10-30 13:25:01 +0100

    Update AppTest.java

--- RECURSOS/LABORATORIO 7/src/test/java/com/gfg/AppTest.java
+++ RECURSOS/LABORATORIO 7/src/test/java/com/gfg/AppTest.java
@@ -12,11 +12,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class AppTest {
-
+    @Parameters("browser")
+    @BeforeMethod
+    public void setup(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver2 = new FirefoxDriver();
+        }
+    }
+    
     @Test
     public void testLoginInChrome() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
@@ -25,12 +34,11 @@ public class AppTest {
     }
     @Test
     public void testLoginInFirefox() {
-        WebDriverManager.firefoxdriver().setup();
-        WebDriver driver2 = new FirefoxDriver();
         driver2.get("https://www.saucedemo.com/");
         driver2.findElement(By.id("user-name")).sendKeys("standard_user");
         driver2.findElement(By.id("password")).sendKeys("secret_sauce");
         driver2.findElement(By.id("login-button")).click();
         driver2.quit();
     }
+
 }
